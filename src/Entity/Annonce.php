b419feeb4beee32c4e403f0b0ab7a49b04ccd414
David <pro.davidsaoud@gmail.com>
2020-04-10 14:11:32 +0200

    add: Image entity (relates to Annonce)

--- src/Entity/Annonce.php
+++ src/Entity/Annonce.php
@@ -2,6 +2,8 @@
 
 namespace App\Entity;
 
+use Doctrine\Common\Collections\ArrayCollection;
+use Doctrine\Common\Collections\Collection;
 use Doctrine\ORM\Mapping as ORM;
 
 /**
@@ -45,6 +47,16 @@ class Annonce
      * @ORM\Column(type="string", length=255, nullable=true)
      */
     private $imageCouverture;
+
+    /**
+     * @ORM\OneToMany(targetEntity="App\Entity\Image", mappedBy="annonce")
+     */
+    private $images;
+
+    public function __construct()
+    {
+        $this->images = new ArrayCollection();
+    }
 
     public function getId(): ?int
     {
@@ -118,6 +130,37 @@ class Annonce
     public function setImageCouverture(?string $imageCouverture): self
     {
         $this->imageCouverture = $imageCouverture;
+
+        return $this;
+    }
+
+    /**
+     * @return Collection|Image[]
+     */
+    public function getImages(): Collection
+    {
+        return $this->images;
+    }
+    
+    public function addImage(Image $image): self
+    {
+        if (!$this->images->contains($image)) {
+            $this->images[] = $image;
+            $image->setAnnonce($this);
+        }
+
+        return $this;
+    }
+
+    public function removeImage(Image $image): self
+    {
+        if ($this->images->contains($image)) {
+            $this->images->removeElement($image);
+            // set the owning side to null (unless already changed)
+            if ($image->getAnnonce() === $this) {
+                $image->setAnnonce(null);
+            }
+        }
 
         return $this;
     }
