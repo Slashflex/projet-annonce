293e6b618468d02207997295c89ac422c1181134
David <pro.davidsaoud@gmail.com>
2020-04-10 15:29:26 +0200

    fix: missing value nullable

--- src/Entity/Annonce.php
+++ src/Entity/Annonce.php
@@ -24,7 +24,7 @@ class Annonce
     private $titre;
 
     /**
-     * @ORM\Column(type="string", length=255)
+     * @ORM\Column(type="string", length=255, nullable=true)
      */
     private $slug;
 
