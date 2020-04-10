579d8b199efc45a8f666a9dfcb93e0bb69147679
David <pro.davidsaoud@gmail.com>
2020-04-10 15:29:11 +0200

    add: faker

--- src/DataFixtures/AppFixtures.php
+++ src/DataFixtures/AppFixtures.php
@@ -2,6 +2,9 @@
 
 namespace App\DataFixtures;
 
+use Faker\Factory;
+use App\Entity\Annonce;
+use App\Entity\Image;
 use Doctrine\Bundle\FixturesBundle\Fixture;
 use Doctrine\Common\Persistence\ObjectManager;
 
@@ -9,8 +12,32 @@ class AppFixtures extends Fixture
 {
     public function load(ObjectManager $manager)
     {
-        // $product = new Product();
-        // $manager->persist($product);
+        for ($i = 1; $i <= 20; $i++) 
+        {
+            $faker = Factory::create('fr_FR');
+            $annonce = new Annonce();
+            $titre = $faker->sentence();
+            $imageCouverture = "https://i.picsum.photos/id/". mt_rand(0, 1000) ."/1200/350.jpg";
+            $introduction = $faker->paragraph(2);
+
+            $annonce
+                ->setTitre($titre)
+                ->setImageCouverture($imageCouverture)
+                ->setIntroduction($introduction);
+            
+            for ($j = 1; $j <= mt_rand(1, 10); $j++)
+            {
+                $image = new Image();
+                $image
+                    ->setUrl("https://i.picsum.photos/id/". mt_rand(0, 1000) ."/640/480.jpg")
+                    ->setLegende($faker->sentence())
+                    ->setAnnonce($annonce->getId());
+                
+                $manager->persist($image);
+            }    
+
+            $manager->persist($annonce);
+        }
 
         $manager->flush();
     }
