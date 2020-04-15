f488cee5278f04208a7ad2e1d6f5628f47fc5140
David <pro.davidsaoud@gmail.com>
2020-04-15 16:02:21 +0200

    add: form builder

--- src/Form/AnnonceType.php
+++ src/Form/AnnonceType.php
@@ -4,6 +4,10 @@ namespace App\Form;
 
 use App\Entity\Annonce;
 use Symfony\Component\Form\AbstractType;
+use Symfony\Component\Form\Extension\Core\Type\MoneyType;
+use Symfony\Component\Form\Extension\Core\Type\TextareaType;
+use Symfony\Component\Form\Extension\Core\Type\TextType;
+use Symfony\Component\Form\Extension\Core\Type\UrlType;
 use Symfony\Component\Form\FormBuilderInterface;
 use Symfony\Component\OptionsResolver\OptionsResolver;
 
@@ -12,13 +16,28 @@ class AnnonceType extends AbstractType
     public function buildForm(FormBuilderInterface $builder, array $options)
     {
         $builder
-            ->add('titre')
+            ->add('titre', TextType::class, [
+                    'attr' => ['placeholder' => 'Titre de votre annonce']
+                ])
             // ->add('slug')
-            ->add('prix')
-            ->add('introduction')
-            ->add('contenu')
-            ->add('imageCouverture')
-        ;
+            ->add('prix', MoneyType::class, [
+                'attr' => ['placeholder' => 'Prix de votre annonce']
+            ])
+            ->add('introduction', TextareaType::class, [
+                'attr' => [
+                    'placeholder' => 'Introduction de votre annonce'
+                ]
+            ])
+            ->add('contenu', TextareaType::class, [
+                'attr' => [
+                    'placeholder' => 'Contenu de votre annonce'
+                ]
+            ])
+            ->add('imageCouverture', UrlType::class, [
+                'attr' => [
+                    'placeholder' => "URL de l'image de votre annonce"
+                ]
+            ]);
     }
 
     public function configureOptions(OptionsResolver $resolver)
