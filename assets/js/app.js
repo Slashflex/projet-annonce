8d3f7ed52f65a6d68c2273cfb0f8becacadd5959
David <pro.davidsaoud@gmail.com>
2020-04-27 17:25:37 +0200

    fix: console.log deleted

--- assets/js/app.js
+++ assets/js/app.js
@@ -10,5 +10,3 @@ import '../scss/global.scss';
 
 // Need jQuery? Install it with "yarn add jquery", then uncomment to import it.
 // import $ from 'jquery';
-
-console.log('Hello Webpack Encore! Edit me in assets/js/app.js');
